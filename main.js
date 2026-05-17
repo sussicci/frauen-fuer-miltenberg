bbaa05ced8f7aa42db66908a465d31cfdb17a996
sussicci <susan.ranchber@hotmail.com>
2026-05-17 22:20:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 import { initCounter } from "./counter.js";
+import { initArbeit } from "./arbeit.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    initCounter({ target: 39 });
+
+    // Counter nur starten, wenn vorhanden
+    if (document.querySelector("#counter")) {
+        initCounter({ target: 39 });
+    }
+
+    // Arbeit nur starten, wenn Grid existiert
+    if (document.querySelector(".unserearbeit")) {
+        initArbeit();
+    }
+
 });
