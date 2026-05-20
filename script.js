99180ddc3c8ed20cbbdb8e2bc030c562e5dc1c4d
sussicci <susan.ranchber@hotmail.com>
2026-05-20 13:43:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const showLessBtn = document.getElementById("showLessBtn");
     const filters = document.querySelectorAll('input[name="filter"]');
 
-    let activeFilter = "aktiv";
+    let activeFilter = "all";
         let page = 1;
 
     function getFilteredItems() {
